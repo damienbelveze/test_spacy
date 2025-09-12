0e4abe3248d18bfebe7a4ffc653429513aaeaa61
damienbelveze <damien.belveze@univ-rennes.fr>
2025-09-12 17:24:58 +0200

    second commit

--- ner.py
+++ ner.py
@@ -19,37 +19,36 @@ import glob
 import os
 import csv
 
-# Define the directory path
+# Definit le dossier source
 directory_path = 'docs'
 
-# Get all file paths in the directory
+# récupère tous les chemins de fichiers dans le dossier source
 file_paths = glob.glob(os.path.join(directory_path, '*'))
 
-# List to store the results
+# crée une liste vide 'results'
 results = []
 
-# Process each file
+# pour chaque fichier dans 'docs"
 for file_path in file_paths:
-    with open(file_path, 'r') as file:
-        content = file.read()
-        nlp = spacy.load('en_core_web_sm')
-        doc = nlp(content)
+    with open(file_path, 'r') as file: # ouvre le fichier en lecture
+        content = file.read() # lit le contenu du fichier et l'envoie dans la variable 'content'
+        nlp = spacy.load('en_core_web_sm') # exécute la fonction nlp de SpaCy à partir du modèle EN (anglais) préalablement téléchargé dans l'environnement virtuel 
+        doc = nlp(content) # crée une variable doc qui comporte le contenu de la variable contents après traitement par la fonction nlp de SpaCy
         for ent in doc.ents:
-            results.append((ent.text, ent.start_char, ent.end_char, ent.label_))
+            results.append((ent.text, ent.start_char, ent.end_char, ent.label_)) # extrait les entités nommées de 'doc' en 4 valeurs (envoie le tout dans la variable 'results') : le texte de l'entité nommée, le numéro du premier caractère, le numéro du dernier caractère, le type d'entité nommée
 
-# Define the CSV file path
+# définit un chemin et un nom pour le fichier CSV de sortie
 csv_file_path = 'entities.csv'
 
-# Write the results to a CSV file
-with open(csv_file_path, 'w', newline='', encoding='utf-8') as csvfile:
-    fieldnames = ['Entity Text', 'Start Char', 'End Char', 'Label']
-    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+# envoie le résultat du traitement vers le fichier CSV de sortie
+with open(csv_file_path, 'w', newline='', encoding='utf-8') as csvfile: # ouvre un fichier CSV formaté en écriture et formaté en utf-8
+    fieldnames = ['Entity Text', 'Start Char', 'End Char', 'Label'] # crée les entêtes de colonnes du CSV d'après les 4 valeurs de 'results'
 
     writer.writeheader()
-    for result in results:
-        writer.writerow({'Entity Text': result[0], 'Start Char': result[1], 'End Char': result[2], 'Label': result[3]})
+    for result in results: 
+        writer.writerow({'Entity Text': result[0], 'Start Char': result[1], 'End Char': result[2], 'Label': result[3]}) #remplit les colonnes du CSV avec les valeurs issues de results, pour Python le premier nombre d'une énumération est 0
 
-print(f"Results have been written to {csv_file_path}")
+print(f"les résultats sont disponibles sur {csv_file_path}") #imprime un message indiquant que le traitement s'est bien déroulé
 
 
 """ legend
